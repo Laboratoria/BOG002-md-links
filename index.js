1337ac565afc6799572f49b5b3bd1da905bbcc3d
dsandovalm <58996746+dsandovalm@users.noreply.github.com>
2021-06-02 10:43:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 module.exports = () => {
-  // ...
+  mdLinks(path, options) {
+    if(options.validate) {
+      /* 
+      href: URL encontrada.
+      text: Texto que aparecía dentro del link (<a>).
+      file: Ruta del archivo donde se encontró el link.
+      status: Código de respuesta HTTP.
+      ok: Mensaje fail en caso de fallo u ok en caso de éxito.
+      */
+    } else {
+      /*
+      href: URL encontrada.
+      text: Texto que aparecía dentro del link (<a>).
+      file: Ruta del archivo donde se encontró el link.
+      */
+    }
+  
+  }
+  // La función debe retornar una promesa (Promise) que resuelva a un arreglo (Array) de objetos (Object)
 };
