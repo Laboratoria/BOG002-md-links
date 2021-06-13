e913627ce4e59017aee8df9b4a491d9d70f0d56b
Nelida Valero <nelidavalero06@gmail.com>
2021-06-13 15:44:00 -0500

    se lee el archivo .md y se obtienen las etiquetas html

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-module.exports = () => {
-  // ...
-};
+// module.exports = () => {
+
+// };
+// habilitar todo
+// var  md  =  require ( 'markdown-it' ) ( {
+//   html : true ,
+//   linkify : true ,
+//   typographer : true
+// } ) ;
+const md = require('markdown-it')();
+
+const fs = require('fs')
+//verificacion
+fs.readFile('./README.md', 'utf8' , (err, data) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  const resultado = md.render(data);
+  // console.log(data)
+  console.log(resultado)
+})
+
+// const tokens = md.parse('[google](www.google.com)')
+// console.log(tokens);
