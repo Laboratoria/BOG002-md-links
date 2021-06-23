7cbfb8623db8cc99bd40fbcac5a88ee39fb21788
leysanchezb92 <leysanchezb92@hotmail.com>
2021-06-23 16:04:04 -0500

    function validate working

---
+++ functions.js
@@ -0,0 +1,90 @@
+const file = './carpeta'
+const fs = require('fs');
+const markdownLinkExtractor = require('markdown-link-extractor');
+const recursion = require('./recursion.js');
+const { default: fetch } = require('node-fetch');
+
+
+// recorrer array de links y evaluar file
+const pathAbs= recursion.evaluatePath(file);
+const pathMd= recursion.mdExt(pathAbs)
+
+// read file//
+const reader = (route)=>{
+  return new Promise((resolve,reject)=>{
+    fs.readFile(route, 'utf8' , (error, fileInfo) => { 
+      if(fileInfo){
+        resolve({ fileInfo, route })
+      } else{
+        reject(error)
+      }
+    });
+  });
+}
+//MDLinks--> path
+pathMd.forEach((route)=>{
+  reader(route)
+    .then(data =>{
+    createObjLink(data)
+    validate(data)
+    })
+    .catch((error => console.log('soy el error',{error})))
+})
+// Crear array de objetos con links//
+function createObjLink(data){
+  let arrLink=[]
+  const links = markdownLinkExtractor(data.fileInfo, true);
+  // if para comprobar los links
+  links.forEach(link => {
+    if(link.href.startsWith('http')){
+      const getLink= link.href;
+      const text = link.text;
+      const objLink= {
+        link: getLink,
+        text: text,
+        file: data.route,
+      }
+      arrLink.push(objLink)
+    }
+  });
+  return (arrLink)
+}
+
+
+//validate
+
+function validate(data){
+  const arrayLinks=createObjLink(data)
+  const arrayPromise= arrayLinks.map((element)=>fetch(element.link)
+    .then((res) => {
+      if (res.status === 200) {
+        console.log({
+          ...element,
+          status: res.status,
+          statusText: res.statusText,
+        })
+      } else {
+          console.log({
+            ...element,
+            status: res.status,
+            statusText: 'FAIL',
+          })
+      }
+    })
+    .catch(() => {
+      return{
+        ...element,
+        status: 'Error',
+        statusText: 'FAIL',
+      }
+    })
+  )
+}
+
+
+
+module.exports = {
+  createObjLink,
+  reader,
+  validate,
+};
--- index.js
+++ index.js
@@ -1,101 +1,19 @@
 const file = './carpeta'
-const path = require('path');
-const { resolve } = require('path');
-const fs = require('fs');
-const markdownLinkExtractor = require('markdown-link-extractor');
 const recursion = require('./recursion.js');
-const { default: fetch } = require('node-fetch');
-// const fetch = require('node-fetch');
+const allFunctions= require('./functions.js')
 
-// // funcion mdLinks
 
-// const mdLinks=(path,option){
-
-// }
-
-// recorrer array de links y evaluar file
-const pathAbs= recursion.evaluatePath(file);
-const pathMd= recursion.mdExt(pathAbs)
-
-// read file//
-const reader = (route)=>{
-  return new Promise((resolve,reject)=>{
-    fs.readFile(route, 'utf8' , (error, fileInfo) => { 
-      if(fileInfo){
-        resolve({ fileInfo, route })
-      } else{
-        reject(error)
-      }
-    });
-  });
+function mdLinks(file){ //---->> TODO Revisar la validacion del la funcion validate, pues debe dar opcion con vomando 
+  // recorrer array de links y evaluar file
+  const pathAbs= recursion.evaluatePath(file);
+  const pathMd= recursion.mdExt(pathAbs)
+  pathMd.forEach((route)=>{
+    allFunctions.reader(route)
+      .then(data =>{
+      allFunctions.createObjLink(data)
+      allFunctions.validate(data)
+      })
+      .catch((error => console.log('soy el error',{error})))
+  })
 }
-//MDLinks--> path
-pathMd.forEach((route)=>{
-  reader(route)
-    .then(data =>{
-    createObjLink(data)
-    validate(data)
-    })
-    .catch((error => console.log('soy el error',{error})))
-})
-// Crear array de objetos con links//
-function createObjLink(data){
-  let arrLink=[]
-  const links = markdownLinkExtractor(data.fileInfo, true);
-  // if para comprobar los links
-  links.forEach(link => {
-    if(link.href.startsWith('http')){
-      const getLink= link.href;
-      const text = link.text;
-      const objLink= {
-        link: getLink,
-        text: text,
-        file: data.route,
-      }
-      arrLink.push(objLink)
-    }
-  });
-  return (arrLink)
-}
-
-
-//validate
-
-function validate(data){
-  const arrayLinks=createObjLink(data)
-  const arrayPromise= arrayLinks.map((element)=>fetch(element.link)
-    .then((res) => {
-      if (res.status === 200) {
-        console.log(res.status)
-        console.log(res.statusText)
-        console.log(element)
-        return{
-          ...element,
-          status: res.status,
-          statusText: res.statusText,
-        }
-      } else {
-          return{
-            ...element,
-            status: res.status,
-            statusText: 'FAIL',
-          }
-      }
-    })
-    .catch(() => {
-      return{
-        ...element,
-        status: 'Error',
-        statusText: 'FAIL',
-      }
-    })
-  )
-  return (arrayPromise)
-}
-
-
-
-module.exports = {
-  createObjLink,
-  reader,
-};
+console.log('hola',mdLinks(file))
