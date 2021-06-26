e209fa78e9aa5e8a2c62e260905d1d5f6baf4224
Nelida Valero <nelidavalero06@gmail.com>
2021-06-26 15:50:11 -0500

    se lee el archivo, pasando por todos los filtros (si el archivo existe, si el una ruta absoluta o relativa, si es un archivo o un directorio), retornando el array de objetos por cada link

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-// module.exports = () => {
-
-// };
+// module.exports = () => { };
 
 const md = require('markdown-it')();
 const jsdom = require("jsdom");
@@ -8,68 +6,50 @@ const { JSDOM } = jsdom;
 const fs = require('fs');
 const path = require('path');
 
-//Funcion que revisa que la ruta ingresada cumpla con los requerimientos para ser validada.
-// const pathAbsolute = ((ruta) => {
+// Funcion que revisa si la ruta ingresada es absoluta o relativa y si es relativa la convierte en absoluta.
+const pathAbsolute = ((ruta) => {
 
-//   const pathExist = fs.existsSync(ruta)
-//   const isAbsoluteFile = path.isAbsolute(ruta);
+  const pathExist = fs.existsSync(ruta)
+  const isAbsoluteFile = path.isAbsolute(ruta);
 
-//   if (pathExist && isAbsoluteFile) {
-//     console.log('La ruta existe y es absoluta');
-//     return ruta
-//   } else if (pathExist && !isAbsoluteFile) {
-//     console.log('La ruta existe y NO es absoluta');
-//     const pathConvertAbsolut = path.resolve(ruta);
-//     return pathConvertAbsolut;
-//   } else {
-//     console.log("la ruta no existe")
-//   }
+  if (pathExist && isAbsoluteFile) {
+    console.log('La ruta existe y es absoluta');
+    return ruta
+  } else if (pathExist && !isAbsoluteFile) {
+    console.log('La ruta existe y NO es absoluta');
+    const pathConvertAbsolut = path.resolve(ruta);
+    return pathConvertAbsolut;
+  } else {
+    console.log("la ruta no existe")
+  }
+});
 
-// });
-// pathAbsolute("C:/Users/Laboratoria/Documents/Laboratoria bootcamp/BOG002-md-links/README.md")
-// pathAbsolute("README.md")
 
+//Función que revisa si la ruta es un directorio, si es asi crea un array con una lista de los archivos.
 const pathDirectory = ((ruta, fileDirectory) => {
 
   let arrayFilesDirectory = fileDirectory || [];
-  const files = fs.readdirSync(ruta)
+  const fileIsDirectory = fs.lstatSync(ruta).isDirectory()
 
-  files.forEach((file) => {
-    const pathChild = path.join(ruta, file)
-    const fileIsDirectory = fs.lstatSync(pathChild).isDirectory()
-
-    if (fileIsDirectory === true) {
-      // console.log("es un directorio")
-      arrayFilesDirectory = pathDirectory(pathChild, arrayFilesDirectory)
-      // arrayFilesDirectory.push(fs.readdirSync(ruta).map((rutaHijo) => pathDirectory(ruta + '/' + rutaHijo)))
-      console.log(arrayFilesDirectory)
-
-    } else {
-      // console.log("No es un directorio");
-      const extFile = path.extname(pathChild);
-
-      if (extFile === ".md") {
-        arrayFilesDirectory.push(pathChild)
-      }
+  if (fileIsDirectory === false) {
+    console.log("Es un archivo")
+    const extFile = path.extname(ruta);
+    if (extFile === ".md") {
+      console.log("es un archivo MD")
+      arrayFilesDirectory.push(ruta)
     }
 
-  })
-  // if (fileIsDirectory) {
+  } else if (fileIsDirectory === true) {
+    const files = fs.readdirSync(ruta)
+    files.map((file) => {
+      const pathChild = path.join(ruta, file)
+      arrayFilesDirectory = pathDirectory(pathChild, arrayFilesDirectory)
+    })
+  }
 
-  // console.log('La ruta es un directorio');
-  // const pathConvertFile = fs.readdirSync(ruta);
-  // console.log(pathConvertFile)
-  // return pathConvertFile
-
-
-  // }  (!fileIsDirectory){
-  //   console.log('La ruta NO es un directorio');
-  //   return ruta;
-  // }
   return arrayFilesDirectory;
 });
-const resultado = pathDirectory("C:/Users/Laboratoria/Documents/Laboratoria bootcamp/BOG002-md-links/node_modules")
-console.log(resultado)
+
 
 // if (module.parent == undefined) {
 //   // node dirTree.js ~/foo/bar
@@ -77,44 +57,35 @@ console.log(resultado)
 //   console.log(util.inspect(pathDirectory(process.argv[2]), false, null));
 // }
 
-//Leer el archivo .md
-// const ReadMDFile = (ruta) => {
+// Función que lee el archivo .md
+const ReadMDFile = (ruta) => {
 
-//   requirementsFile(ruta)
-//     // .then((resultado) => { console.log(resultado)})
-//     // .catch((error) => { console.log(error) });
+  pathAbsolute(ruta);
+  const arrayListFiles = pathDirectory(ruta);
+  let arrayLinks = []
+  arrayListFiles.forEach((ruta1) => {
 
-//   //Asincrono
-//   // fs.readFile(ruta, 'utf8', (err, data) => {
-//   //   if (err) {
-//   //     console.error(err)
-//   //     return
-//   //   }
-//   //Sincrono
-//   const data = fs.readFileSync(ruta, 'utf8')
-//   const mdHTML = md.render(data);
-//   const dom = new JSDOM(mdHTML);
-//   const etiquetasA = dom.window.document.querySelectorAll("a")
+    const data = fs.readFileSync(ruta1, 'utf8')
+    const mdHTML = md.render(data);
+    const dom = new JSDOM(mdHTML);
+    const etiquetasA = dom.window.document.querySelectorAll("a")
 
-//   let arrayLinks = []
-//   etiquetasA.forEach((link) => {
 
-//     const coincidenciaEnlace = new RegExp('#', 'y');
-//     const attributeHref = link.getAttribute('href');
+    etiquetasA.forEach((link) => {
+      const coincidenciaEnlace = new RegExp('#', 'y');
+      const attributeHref = link.getAttribute('href');
 
-//     if (!coincidenciaEnlace.test(attributeHref)) {
+      if (!coincidenciaEnlace.test(attributeHref)) {
 
-//       arrayLinks.push({
-//         href: link.getAttribute('href'),
-//         text: link.textContent,
-//         file: ruta
-//       })
+        arrayLinks.push({
+          href: link.getAttribute('href'),
+          text: link.textContent,
+          file: ruta1
+        })
+      }
+    })
+  });
 
-//     }
-
-//   })
-
-//   return arrayLinks
-// }
-// // console.log(ReadMDFile("./README.md"));
-// ReadMDFile("README.md")
+  return arrayLinks
+}
+console.log(ReadMDFile("C:/Users/Laboratoria/Documents/Laboratoria bootcamp/BOG002-md-links/README.md"))
